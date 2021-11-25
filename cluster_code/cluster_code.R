361484813ec58ca12d01184bfbc42624e4375175
Rani Basna <ranibasna@yahoo.com>
2021-11-25 10:22:58 +0100

    doing first cluster

---
+++ cluster_code/cluster_code.R
@@ -0,0 +1,12 @@
+args = commandArgs(trailingOnly=TRUE)
+#
+filename <- args[1]
+
+USArrests <- read.csv(file = filename)
+df <- scale(USArrests)
+set.seed(123)
+# we will now apply kmeans method to cluster the data
+#
+km.res <- kmeans(df, 4, nstart = 25)
+clustering_result <- cbind(USArrests, cluster = km.res$cluster)
+write.csv(clustering_result, file = args[2])
